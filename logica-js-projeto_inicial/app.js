f3383b0f2672d0e495151f0d08b9e0697f0ffa72
laurammaria <148969673+laurammaria@users.noreply.github.com>
2024-10-29 22:27:28 -0300

    js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,7 +1,14 @@
 alert("Boas vindas ao jogo do número secreto");
-let  numSecreto = 5;
+
+let  numSecreto = 2;
+
+console.log(numSecreto)
 let chute = prompt("Escolha um número entre 1 e 10");
 
 if (chute == numSecreto ){
-    console.log("Você descobriu o número secreto (5)");
+    alert(`Você descobriu o número secreto ${numSecreto}`);
+} 
+else {
+    alert ('Você errou :(');
+
 }
