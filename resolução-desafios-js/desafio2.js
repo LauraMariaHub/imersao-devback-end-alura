a06265a779766ce6be8e04ea07071aa96e41fec2
laurammaria <148969673+laurammaria@users.noreply.github.com>
2024-10-30 21:46:59 -0300

    desafio 2

---
+++ resolução-desafios-js/desafio2.js
@@ -0,0 +1,33 @@
+let dia = prompt("Qual é o dia da semana?");
+
+if (dia == 'sábado') {
+    alert('Bom final de semana!')
+}
+
+else if (dia == 'domingo')
+    alert('Bom final de semana!')
+
+else
+    alert('Boa semana!')
+
+let num = prompt("Digite um número positivo ou negativo");
+
+if (num > 0) {
+    alert('O número é positivo')
+}
+else
+    alert('O número é negativo')
+
+let pontos = prompt("Digite a pontuação");
+
+if (pontos >= 100) {
+    alert('Parábens, você venceu!')
+}
+else
+    alert('Tente novamente para ganhar.')
+
+let SaldoConta = 500
+alert(`Seu saldo é ${SaldoConta}`)
+
+let nome = prompt('Qual o seu nome?')
+alert(`Boas vindas, ${nome}!`)
