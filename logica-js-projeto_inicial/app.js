20c2039c6b9b1dc6afef8e79793dc3939c7d80fb
laurammaria <148969673+laurammaria@users.noreply.github.com>
2024-10-30 22:03:53 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -3,12 +3,20 @@ alert("Boas vindas ao jogo do número secreto");
 let  numSecreto = 2;
 
 console.log(numSecreto)
-let chute = prompt("Escolha um número entre 1 e 10");
+let chute;
 
-if (chute == numSecreto ){
-    alert(`Você descobriu o número secreto ${numSecreto}`);
-} 
-else {
-    alert ('Você errou :(');
+while (chute != numSecreto) {
+    chute = prompt("Escolha um número entre 1 e 10");
+
+    if (chute == numSecreto) {
+        alert(`Você descobriu o número secreto ${numSecreto}`);
+    }
+    else {
+        if (chute > numSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+    }
 
 }
