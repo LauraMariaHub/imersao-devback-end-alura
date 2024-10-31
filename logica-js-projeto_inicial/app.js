b42516a91305d3846665a7bd2f63f2e06bd51d5c
laurammaria <148969673+laurammaria@users.noreply.github.com>
2024-10-30 22:59:11 -0300

    Update app.js

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,22 +1,32 @@
 alert("Boas vindas ao jogo do número secreto");
 
-let  numSecreto = 2;
+let  numSecreto = parseInt(Math.random() * 100 + 1);
 
-console.log(numSecreto)
+console.log(numSecreto);
 let chute;
+let contador = 1;
 
-while (chute != numSecreto) {
-    chute = prompt("Escolha um número entre 1 e 10");
+while(chute != numSecreto){
+    chute = prompt("Escolha um número entre 1 e 100");
 
-    if (chute == numSecreto) {
-        alert(`Você descobriu o número secreto ${numSecreto}`);
+if (chute == numSecreto ){
+    break;
+} 
+else {
+
+    if(chute > numSecreto){
+        alert(`O número secreto é menor que ${chute}`);
+    } else {
+        alert(`O número secreto é maior que ${chute}`);
     }
-    else {
-        if (chute > numSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
-        } else {
-            alert(`O número secreto é maior que ${chute}`)
-        }
-    }
+
+    contador++;
+}
 
 }
+
+let palavraTentativa = contador > 1 ?'tentivas' : 'tentativa';
+alert(`Você descobriu o número secreto ${numSecreto} com ${contador} ${palavraTentativa}`);
+
+
+
